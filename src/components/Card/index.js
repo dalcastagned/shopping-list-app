01121e0c5b441d02a9cf48441e72b8e4bdde621e
Daniel Dalcastagne <dalcastagned@gmail.com>
2022-01-30 21:13:41 -0300

    update Card Component

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,78 +1,85 @@
 import React, { useContext, useState } from 'react';
-
-import SwipeableViews from 'react-swipeable-views';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
 import { FaCaretRight, FaCaretLeft } from "react-icons/fa";
 import { IoMdTrash } from "react-icons/io";
 import { BsFillCartPlusFill, BsFillCartDashFill } from "react-icons/bs";
-
-
 import * as S from './elements'
 import { ItemContext } from '../../context';
 
 const Card = ({ data }) => {
 
-    const [amount, setAmount] = useState(data.amount)
-    const [item, setItem] = useState(data.item)
-    const [value, setValue] = useState(data.value)
-    const {removeItem, addCart, removeCart} = useContext(ItemContext)
+  const [amount, setAmount] = useState(data.amount)
+  const [item, setItem] = useState(data.item)
+  const [value, setValue] = useState(data.value)
+  const { removeItem, addCart, removeCart } = useContext(ItemContext)
 
-    const handleSubmit = () => {
-    }
+  const handleSubmit = () => {
+  }
 
-    return (
-        <S.Container>
-            <SwipeableViews
-                enableMouseEvents
-                index={1}
-                onChangeIndex={(index) => {
-                    if (index === 0 && !data.inCart) {
-                        addCart(data.id)
-                    }
-                    if (index === 0 && data.inCart) {
-                        removeCart(data.id)
-                    }
-                    if (index === 1) {
-                        return
-                    }
-                    if (index === 2) {
-                        removeItem(data.id)
-                    }
-                }}>
-                <S.Slide1 inCart={data.inCart}>{data.inCart
-                    ? <BsFillCartDashFill />
-                    : <BsFillCartPlusFill />
-                }
-                    <FaCaretRight />
-                </S.Slide1>
-                <S.Slide2>
-                    <S.Information onBlur={handleSubmit}>
-                        <S.Amount inCart={data.inCart}
-                            type='number'
-                            value={amount}
-                            onChange={(event) => {setAmount(event.target.value)}}
-                            placeholder='0'
-                        />
-                        <S.Item inCart={data.inCart}
-                            type='text'
-                            value={item}
-                            onChange={(event) => {setItem(event.target.value)}}
-                            placeholder='Item'
-                        />
-                        <S.Value inCart={data.inCart}>
-                            <p>R$:</p>
-                            <input
-                                type='number'
-                                value={value}
-                                onChange={(event) => {setValue(event.target.value)}}
-                                placeholder='0,00'
-                            />
-                        </S.Value>
-                    </S.Information>
-                </S.Slide2>
-                <S.Slide3><FaCaretLeft /><IoMdTrash /></S.Slide3>
-            </SwipeableViews>
-        </S.Container>
-    );
+  return (
+    <S.Container>
+      <Carousel
+        autoPlay={false}
+        swipeScrollTolerance={150}
+        showArrows={false}
+        selectedItem={1}
+        infiniteLoop={false}
+        showStatus={false}
+        showIndicators={false}
+        swipeable={true}
+        emulateTouch={true}
+        showThumbs={false}
+        onChange={(index) => {
+          if (index === 0 && !data.inCart) {
+            addCart(data.id)
+          }
+          if (index === 0 && data.inCart) {
+            removeCart(data.id)
+          }
+          if (index === 1) {
+            return
+          }
+          if (index === 2) {
+            removeItem(data.id)
+          }
+        }}
+      >
+        <S.Slide1 inCart={data.inCart}>{data.inCart
+          ? <BsFillCartDashFill />
+          : <BsFillCartPlusFill />
+        }
+          <FaCaretRight />
+        </S.Slide1>
+        <S.Slide2>
+          <S.Information onBlur={handleSubmit}>
+            <S.Amount inCart={data.inCart}
+              type='number'
+              value={amount}
+              onChange={(event) => { setAmount(event.target.value) }}
+              placeholder='0'
+            />
+            <S.Item inCart={data.inCart}
+              type='text'
+              value={item}
+              onChange={(event) => { setItem(event.target.value) }}
+              placeholder='Item'
+            />
+            <S.Value inCart={data.inCart}>
+              <p>R$:</p>
+              <input
+                type='number'
+                value={value}
+                onChange={(event) => { setValue(event.target.value) }}
+                placeholder='0,00'
+              />
+            </S.Value>
+          </S.Information>
+        </S.Slide2>
+        <S.Slide3><FaCaretLeft /><IoMdTrash /></S.Slide3>
+      </Carousel>
+    </S.Container>
+  )
 }
 
 export default Card;
